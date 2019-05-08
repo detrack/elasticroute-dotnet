555fe07cda7c96f39ef8fe833faa29301700e81f
chesnutcase <chester8991@gmail.com>
2019-05-08 23:18:07 +0800

    add depots and tests

---
+++ ElasticRoute.Tests/DepotValidationTest.cs
@@ -0,0 +1,136 @@
+using NUnit.Framework;
+using System.Reflection;
+using Detrack.ElasticRoute;
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Tests
+{
+    public class DepotValidationTest
+    {
+        private static Random rng = new Random();
+
+        public Depot CreateDepot(string testName = null, [System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
+        {
+            Depot depot = new Depot();
+            depot.Name = testName ?? memberName + System.DateTime.Now.Ticks;
+            string[] testAddresses =
+            { "61 Kaki Bukit Ave 1 #04-34, Shun Li Ind Park Singapore 417943",
+            "8 Somapah Road Singapore 487372",
+            "80 Airport Boulevard (S)819642",
+            "80 Mandai Lake Road Singapore 729826",
+            "10 Bayfront Avenue Singapore 018956",
+            "18 Marina Gardens Drive Singapore 018953"
+            };
+            depot.Address = testAddresses[rng.Next(testAddresses.Length)];
+            return depot;
+        }
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestMustHaveAtLeastOneDepot()
+        {
+            List<Depot> depots = new List<Depot>();
+            try
+            {
+                Depot.ValidateDepots(depots);
+                Assert.Fail("No exception was thrown");
+            }
+            catch (BadFieldException ex)
+            {
+                Assert.AreEqual("You must have at least one depot", ex.Message);
+            }
+        }
+
+        [Test]
+        public void TestNamesMustBeDistinct()
+        {
+            List<Depot> depots = new List<Depot>{
+                this.CreateDepot(),
+                this.CreateDepot("bad"),
+                this.CreateDepot("bad")
+            };
+            try
+            {
+                Depot.ValidateDepots(depots);
+                Assert.Fail("No exception was thrown");
+            }
+            catch (BadFieldException ex)
+            {
+                Assert.AreEqual("Depot name must be distinct", ex.Message);
+            }
+            depots = new List<Depot>{
+                this.CreateDepot(),
+                this.CreateDepot()
+            };
+            Assert.True(Depot.ValidateDepots(depots));
+        }
+
+        [Test]
+        public void TestNamesCannotBeNull([Values(null, "", " ")]string testName)
+        {
+            Depot depot = new Depot();
+            try
+            {
+                depot.Name = null;
+                Assert.Fail("No exception was thrown");
+            }
+            catch (BadFieldException ex)
+            {
+                Assert.AreEqual("Depot name cannot be null", ex.Message);
+            }
+        }
+
+        [Test]
+        public void TestNamesCannotBeLongerThan255Chars()
+        {
+            string longName = new string('A', 256);
+            Depot depot = new Depot();
+            try
+            {
+                depot.Name = longName;
+                Assert.Fail("No exception was thrown");
+            }
+            catch (BadFieldException ex)
+            {
+                Assert.AreEqual("Depot name cannot be more than 255 chars", ex.Message);
+            }
+        }
+
+        [Test]
+        public void TestCanPassCoordinatesOnly()
+        {
+            List<Depot> depots = new List<Depot>
+            {
+                this.CreateDepot(),
+                this.CreateDepot()
+            };
+            depots[1].Address = null;
+            depots[1].Lat = 1.3368888888888888f;
+            depots[1].Lng = 103.91086111111112f;
+            Assert.IsTrue(Depot.ValidateDepots(depots));
+        }
+
+        [Test]
+        public void TestCannotPassNoFormsOfAddress()
+        {
+            List<Depot> depots = new List<Depot>
+            {
+                this.CreateDepot()
+            };
+            depots[0].Address = null;
+            try
+            {
+                Depot.ValidateDepots(depots);
+                Assert.Fail("No exception was thrown");
+            }catch(BadFieldException ex){
+                Assert.AreEqual("Depot address and coordinates are not given", ex.Message);
+            }
+        }
+    }
+}
---
+++ ElasticRoute/Depot.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Detrack.ElasticRoute
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Depot
+    {
+#pragma warning disable CS0169, IDE0051 // Remove unused private members
+        private string _name;
+#pragma warning restore CS0169, IDE0051 // Remove unused private members
+
+        [JsonProperty("name")]
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == null || value.Trim() == "")
+                {
+                    throw new BadFieldException("Depot name cannot be null");
+                }
+                else if (value.Length > 255)
+                {
+                    throw new BadFieldException("Depot name cannot be more than 255 chars");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+        [JsonProperty("address")]
+        public string Address { get; set; }
+        [JsonProperty("postal_code")]
+        public string PostalCode { get; set; }
+        [JsonProperty("lat")]
+        public float? Lat { get; set; }
+        [JsonProperty("lng")]
+        public float? Lng { get; set; }
+        [JsonProperty("default")]
+        public bool? Default { get; set;}
+
+        public Depot()
+        {
+        }
+
+        public static bool ValidateDepots(List<Depot> depots)
+        {
+            if (depots.Count < 1)
+            {
+                throw new BadFieldException("You must have at least one depot");
+            }
+            foreach (Depot depot in depots)
+            {
+                bool sameNameDepot(Depot v) => v.Name == depot.Name;
+                if (depots.FindAll((Predicate<Depot>)sameNameDepot).Count > 1)
+                {
+                    throw new BadFieldException("Depot name must be distinct");
+                }
+                if (depot.Lat == null || depot.Lng == null)
+                {
+                    if (depot.Address == null)
+                    {
+                        throw new BadFieldException("Depot address and coordinates are not given");
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
